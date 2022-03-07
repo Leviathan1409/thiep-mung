d6cd5ce511fb5d5cc16d1c2b88f77437cd6f439c
Leviathan1409 <101087677+Leviathan1409@users.noreply.github.com>
2022-03-07 14:56:47 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em yêu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng người yêu anh';
+const contentCard = 'Chúc cô bé của anh 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau, anh yêu em mình cùng bên nhau thật lâu em nhé!';
+
+const giftImage = '274328844_632594027985322_5422425180009905741_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
